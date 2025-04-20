5165dc26733f942b0ce58ab0f5893b42fed08347
katyalika <lihachevak05@gmail.com>
2025-04-20 16:38:18 +0300

    start project
    созданы базовые классы и пакеты, добавлены зависимости для Faker и Swing в pom.xml

---
+++ SayronArmy/src/main/java/com/mycompany/sayronarmy/SayronArmy.java
@@ -0,0 +1,11 @@
+package com.mycompany.sayronarmy;
+
+/**
+ *
+ * @author lihac
+ */
+public class SayronArmy {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
