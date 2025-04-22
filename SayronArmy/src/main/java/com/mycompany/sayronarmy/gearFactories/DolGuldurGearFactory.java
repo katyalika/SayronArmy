a9068bcab8cc5dde5b2d9ff45b90061b8da53260
katyalika <lihachevak05@gmail.com>
2025-04-22 10:01:28 +0300

    factory and builders update

---
+++ SayronArmy/src/main/java/com/mycompany/sayronarmy/gearFactories/DolGuldurGearFactory.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sayronarmy.gearFactories;
+
+/**
+ *
+ * @author lihac
+ */
+public class DolGuldurGearFactory implements OrkGearFactory{
+    @Override 
+    public String createWeapon() {
+        return "Копье";
+    }
+    @Override 
+    public String createArmor() {
+        return "Кольчуга";
+    }
+    @Override 
+    public String createBanner() {
+        return "Знамя с пауком";
+    }
+}
---
+++ SayronArmy/src/main/java/com/mycompany/sayronarmy/gearFactories/MistyMountainsGearFactory.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sayronarmy.gearFactories;
+
+/**
+ *
+ * @author lihac
+ */
+public class MistyMountainsGearFactory implements OrkGearFactory {
+    
+}
